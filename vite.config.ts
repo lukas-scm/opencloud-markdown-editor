20e3bc47f4250102cf7ba249beef0f29aa07a2c6
lukas-scm <50322086+lukas-scm@users.noreply.github.com>
2026-05-28 01:30:16 +0700

    chore: dependency updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   name: 'markdown-editor',
-  plugins: [Icons({ compiler: 'vue3' })],
-  build: {
-    rollupOptions: {
-      // fix for windows path slashes
-      output: {
-        entryFileNames: 'js/[name]-[hash].js'
-      }
-    }
-  }
+  plugins: [Icons({ compiler: 'vue3' })]
 })
